8e6b7a1db4bfd400e8a8fa035e01f39e54d5cce8
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-04-26 21:12:12 +0100

    AQOLAGP-108 updated schema integrity validation with property tables

--- database/init/schema_integrity_validation.sql
+++ database/init/schema_integrity_validation.sql
@@ -7,7 +7,9 @@ AND table_name IN (
     'postcodes', 
     'crime_data', 
     'spatial_ref_sys',
-    'school_data'
+    'school_data',
+    'property_data',
+    'property_transactions'
 );
 
 -- Query to check the correct columns, data types are present in each table and NULL constraints enforced
@@ -22,7 +24,9 @@ AND table_name IN (
     'lsoas',
     'postcodes',
     'crime_data',
-    'school_data')
+    'school_data',
+    'property_data',
+    'property_transactions')
 ORDER BY table_name, column_name;
 
 -- Query to check the relational constraints (Primary and Foreign Keys)
@@ -36,6 +40,12 @@ JOIN information_schema.key_column_usage AS kcu
     ON tc.constraint_name = kcu.constraint_name 
     AND tc.table_schema = kcu.table_schema
 WHERE tc.table_schema = 'public' 
-AND tc.table_name IN ('lsoas', 'postcodes', 'crime_data', 'school_data')
+AND tc.table_name IN (
+    'lsoas',
+    'postcodes',
+    'crime_data', 
+    'school_data', 
+    'property_data',
+    'property_transactions')
 AND tc.constraint_type IN ('PRIMARY KEY', 'FOREIGN KEY')
 ORDER BY tc.table_name;
