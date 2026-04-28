309ada3778f45d4670e320ec10fdd2aa593e48bf
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-04-28 09:28:00 +0100

    AQOLAGP-108 added lat and long to property_data

--- database/init/initialise.sql
+++ database/init/initialise.sql
@@ -77,13 +77,15 @@ CREATE TABLE IF NOT EXISTS school_data (
 
 CREATE TABLE IF NOT EXISTS property_data (
     property_id SERIAL PRIMARY KEY, -- uprn can be used for this
-    paon VARCHAR(100),  -- House number/name
+    paon VARCHAR(100) NOT NULL,  -- Primary Addressable Object Name. Typically the house number or name.
     saon VARCHAR(100),  -- Flat/Unit number
     street VARCHAR(255) NOT NULL,
     full_address TEXT NOT NULL,        
     postcode VARCHAR(10) REFERENCES postcodes(postcode) ON DELETE CASCADE,
     property_type CHAR(1) NOT NULL,   -- D, S, T, F, O
-    boundary GEOMETRY(MULTIPOLYGON, 4326) NOT NULL
+    boundary GEOMETRY(MULTIPOLYGON, 4326) NOT NULL,
+    latitude DECIMAL(9,6) NOT NULL,
+    longitude DECIMAL(9,6) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS property_transactions (
