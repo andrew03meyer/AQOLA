9268b69c8e443b6fd5e8fb255c2a0b1f72eb1bc0
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-05-04 22:47:49 +0100

    AQOLAGP-108 completed cleansing scripts and ingestion for property data and transactions

--- backend/data/manager.py
+++ backend/data/manager.py
@@ -12,7 +12,6 @@ from cleansing_scripts.property_transactions import property_transactions_proces
 from cleansing_scripts.flood_occurrence_cleansing import flood_occurance_process
 from cleansing_scripts.property_cleansing import property_process
 from cleansing_scripts.property_transactions import property_transactions_process
-from cleansing_scripts.flood_occurrence_cleansing import flood_occurance_process
 from ingestion import initialise_db, ingest_table, get_rows, get_row_count
 from pathlib import Path
 from testing.reference_checks import reference_check_process
@@ -58,14 +57,7 @@ def run_csv_creation(missingCSVs):
         # print("=======================================================================")
         print("Creating Flood Data CSV...")
         flood_process()
-    if "property_data" in missingCSVs:
-        # print("=======================================================================")
-        print("Creating Property Data CSV...")
-        property_process()
-    if "property_transactions_data" in missingCSVs:
-        # print("=======================================================================")
-        print("Creating Property Transactions Data CSV...")
-        property_transactions_process()    
+
     if "flood_occurrences" in missingCSVs:
         # print("=======================================================================")
         print("Creating Flood Occurrences CSV...")
@@ -78,10 +70,7 @@ def run_csv_creation(missingCSVs):
         # print("=======================================================================")
         print("Creating Property Transactions Data CSV...")
         property_transactions_process()    
-    if "flood_occurrences" in missingCSVs:
-        # print("=======================================================================")
-        print("Creating Flood Occurrences CSV...")
-        flood_occurance_process()        
+        
 
 # ensures that the LSOA CSV is present, and if there's more than one to be ingest, that both the LSOA and postcodes table are there
 def ingest_process(dataPath):
@@ -134,19 +123,8 @@ def run_ingest(ingestCSVs, dataPath):
             ingestCSVs.remove(property_data_path)
         get_row_count("property_data")
 
-    # Ingest property data here because it is a parent table to property_transactions (referential integrity)
-    property_data_path = dataPath / "property_data" / "property_data.csv"
-    if property_data_path.exists():
-        print("=======================================================================")
-        print("Ingesting property_data data...")
-        ingest_table(property_data_path, "property_data", filtered_postcodes)
-        
-        # Remove from list so loop does not ingest it again
-        if property_data_path in ingestCSVs: 
-            ingestCSVs.remove(property_data_path)
-        get_row_count("property_data")
 
-        
+
     # then ingest everything else
     for csv in ingestCSVs:
         print("=======================================================================")
