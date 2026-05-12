aa5fc2e3afbe26dacb3c28a77a6e631925f046bb
andrew meyer <andrew.meyer003@gmail.com>
2026-05-12 15:03:34 +0100

    feat: documentation layout done

--- frontend/aqola-frontend/app/docs/contents/ContentsBar.tsx
+++ frontend/aqola-frontend/app/docs/contents/ContentsBar.tsx
@@ -5,7 +5,7 @@ const ContentsBar = () => {
     const [h1List, setH1List] = useState<HTMLElement[]>([]);
     useEffect(() => {
         const h1Elements = document.querySelectorAll("h2");
-        setH1List(Array.from(h1Elements).filter((h1) => !h1.textContent?.includes("Contents")));
+        setH1List(Array.from(h1Elements).filter((h2) => !h2.textContent?.includes("Contents")));
     }, []);
 
     return (
@@ -15,9 +15,9 @@ const ContentsBar = () => {
             </div>
             <div className="contents-list">
                 <ul id="contents-list">
-                    {Array.from(h1List).map((h1, index) => (
+                    {Array.from(h1List).map((h2, index) => (
                         <li key={index}>
-                            <a href={`#${h1.id}`}>{h1.textContent}</a>
+                            <a href={`#${h2.id}`}>{h2.textContent}</a>
                         </li>
                     ))}
                 </ul>
--- frontend/aqola-frontend/app/docs/contents/docs.tsx
+++ frontend/aqola-frontend/app/docs/contents/docs.tsx
@@ -3,81 +3,162 @@ import { OctagonAlert } from "lucide-react";
 
 const Content = () => {
     return (
-        <div style={{ height: "100%" }}>
+        <div className="docs-page">
+
+            {/* ── Heading — natural height, never overlaps ── */}
             <div className="heading">
-                <div className="tag">Documentation</div>
-                <h1>Welcome to the AQOLA Documentation!</h1>
-                <p style={{width: "75%"}}>
-                    This page is here to help you get started with AQOLA; and to provide information on it's features, the processes and the data used. AQOLA is designed to provide you (the user) with an ability to show real life datasets, in a readable format.
-                    Whether this is performing processed to raw data, or just displaying it in a chart, AQOLA is here to help you with that. We hope you enjoy using it as much as we enjoyed building it!
-                </p>
+                <div className="flex-row-centered">
+                    <img src="/koala.png" style={{width: "70px", borderRadius:"20px"}} />
+                    <div className="tag" style={{margin:"20px"}}>Documentation</div>
+                </div>
             </div>
 
+            {/* ── Main content row — fills remaining space ── */}
             <div className="docs-content">
                 <ContentsBar />
 
                 <div className="contents-text">
 
+                    {/* Introduction */}
                     <div className="contents-items">
-                        {/* <!-- Getting Started --> */}
+                        <h2 id="introduction">Introduction</h2>
+                        <div>
+                            This page is here to help you get started with AQOLA; and to provide
+                            information on its features, the processes and the data used. AQOLA is
+                            designed to provide you (the user) with an ability to show real life
+                            datasets, in a readable format. Whether this is performing processes to
+                            raw data, or just displaying it in a chart, AQOLA is here to help you
+                            with that. We hope you enjoy using it as much as we enjoyed building it!
+                        </div>
+                    </div>
+                    {/* Getting Started */}
+                    <div className="contents-items">
                         <h2 id="getting-started">Getting Started</h2>
-                        <h3>API Documentation</h3>
-                        <p><a href="http://aqola.andrewmeyer.co.uk:8000/docs">API Documentation</a></p>
-                        <h3>Layout</h3>
-                        <p>Here you will find information about the layout of the AQOLA interface and how to navigate through it.</p>
+                        <ul style={{listStyle:"disc"}}>
+                            <li>
+                                <b className="highlight">Interacting with the map</b> is much like Google/Apple maps. Pinch 
+                                 or double tap to zoom, click and drag to move around, click to 
+                                 select locations.
+                            </li>
+                            <li>
+                                <b className="highlight">To change dataset</b> (i.e. looking for Flood risk), go 
+                                to the top left corner of the screen, and click the dropdown
+                                . This should allow you to go between all our available datsets
+                            </li>
+                            <li>
+                                <b className="highlight">To open our graphs</b>, please look at the bottom of your screen, 
+                                where you'll see a bar showing all the available charts for that 
+                                dataset. On hover, you should be able to see the name of the 
+                                chart, which should hopefully give you a good idea of what it's 
+                                for.
+                            </li>
+                            <li>
+                                <b className="highlight">To minimise/close charts</b>, click the "-"/"x" in the top right corner of the 
+                                chart pop-up. To reopen them. Please click on the graph name, on the right 
+                                hand side of the screen. You can also close them from there, using the "x" 
+                                button
+                            </li>
+                            <li>
+                                <b className="highlight">To resize graphs</b>, please hover over the bottom right corner of graphs,
+                                left click and drag.
+                            </li>
+                            <li>
+                                <b className="highlight">To save your work</b>, please click the download icon in the top left.
+                                This will save a local copy of the app state to your PC. You can then 
+                                reload this using the upload button. Please note that the download will 
+                                not show you graphs in any other software, you have to reload it to our 
+                                website.
+                            </li>
+                        </ul>
                     </div>
 
                     <div className="contents-items">
-                        <h2 id="data">Data</h2>
-                        <p>Here you will find information about the data used in AQOLA, how it's processed, and where we obtained it.</p>
-                        <div className="warning">
-                            <OctagonAlert size={40} style={{ marginRight: "5px"}} />
-                            <p><b>WARNING: </b> Our data is not live. It may be outdated.</p>
+                        {/* -------- Terminology ---------- */}
+                        <h2 id="terminology">Terminology</h2>
+                        <div>
+                            <div className="tag"><a href="https://www.ons.gov.uk/methodology/geography/ukgeographies/statisticalgeographies">LSOAS</a> | Lower Layer Super Output Areas</div>
+                            <div style={{fontSize:"10px"}}>Typically contains 1,000–3,000 residents or 400–1,200 households</div>
                         </div>
+                    </div>
+
+                    <div className="contents-items">
+                        {/* -------- API Docs ---------- */}
+                        <h3>API Documentation</h3>
+                        <div>
+                            <a href="http://aqola.andrewmeyer.co.uk:8000/docs">
+                                API Documentation
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* {/* -------- Data Section ---------- */}
+                    <div className="contents-items">
+                        <h2 id="data">Data</h2>
+                        <div>
+                            Here you will find information about the data used in AQOLA, how
+                            it's processed, and where we obtained it.
+                        </div>
+
+                        <div className="warning">
+                            <OctagonAlert size={40} style={{ marginRight: "5px" }} />
+                            <div><b>WARNING: </b> Our data is not live. It may be outdated.</div>
+                        </div>
+
+                        {/* -------- Table description ---------- */}
                         <h3>Data Sources</h3>
-                        <ul>
-                            <li><i>Dataset</i> - what the data is about</li>
-                            <li><i>Source</i> - where the data is obtained from, including a hyperlink to the source</li>
-                            <li><i>Description</i> - a brief description of the dataset, including any relevant details about its content and structure</li>
-                            <li><i>Date Range</i> - the time period covered by the dataset, indicating the start and end dates of the data collection</li>
-                            <li><i>Data Type</i> - the format in which the data is provided (e.g., CSV, JSON, SQL database, etc.)</li>
-                            <li><i>Last Updated</i> - the date when we last updated our local copy of the dataset</li>
+                        <ul style={{listStyle:"disc"}}>
+                            <li><b className="highlight"><i>Dataset</i></b> — what the data is about</li>
+                            <li><b className="highlight"><i>Source</i></b> — where the data is obtained from, including a hyperlink to the source</li>
+                            <li><b className="highlight"><i>Description</i></b> — a brief description of the dataset, including any relevant details about its content and structure</li>
+                            <li><b className="highlight"><i>Date Range</i></b> — the time period covered by the dataset</li>
+                            <li><b className="highlight"><i>Data Type</i></b> — the format in which the data is provided (e.g., CSV, JSON, SQL, etc.)</li>
+                            <li><b className="highlight"><i>Last Updated</i></b> — the date when we last updated our local copy</li>
                         </ul>
+
+
+                        {/* -------- Table of Data Sources ---------- */}
                         <table>
                             <thead>
                                 <tr>
-                                    <th>Dataset</th>
-                                    <th>Source</th>
-                                    <th>Description</th>
-                                    <th>Date Range</th>
-                                    <th>Data Type</th>
-                                    <th>Last Updated</th>
+                                    <th className="highlight">Dataset</th>
+                                    <th className="highlight">Source</th>
+                                    <th className="highlight">Description</th>
+                                    <th className="highlight">Date Range</th>
+                                    <th className="highlight">Data Type</th>
+                                    <th className="highlight">Last Updated</th>
                                 </tr>
                             </thead>
-
-                            {/* <!-- Crime Data --> */}
                             <tbody>
                                 <tr>
                                     <td>Crime</td>
-                                    <td><a href="https://data.police.uk/data/">Goverment Data - Police</a></td>
-                                    <td>Dataset details can be found <a href="https://data.police.uk/about/">here</a>. For our uses, we utilised data from Kent exclusively.</td>
+                                    <td><a href="https://data.police.uk/data/">Government Data — Police</a></td>
+                                    <td>
+                                        Dataset details can be found{" "}
+                                        <a href="https://data.police.uk/about/">here</a>.
+                                        For our uses, we utilised data from Kent exclusively.
+                                    </td>
                                     <td>October 2020 to August 2025</td>
                                     <td>CSV</td>
                                     <td>Late 2025</td>
                                 </tr>
-
-                                {/* <!-- School Data --> */}
                                 <tr>
                                     <td>Schools</td>
-                                    <td><a href="https://www.compare-school-performance.service.gov.uk/schools-by-type?step=default&table=schools&geographic=la&for=primary&region=886&datasetfilter=final">Government Data - Schools</a></td>
-                                    <td>Download <a href="https://www.compare-school-performance.service.gov.uk/download-data">here</a>. For our uses, we utilised data from
-                                        Kent exclusively.</td>
-                                    <td>2010-2025</td>
+                                    <td>
+                                        <a href="https://www.compare-school-performance.service.gov.uk/schools-by-type?step=default&table=schools&geographic=la&for=primary&region=886&datasetfilter=final">
+                                            Government Data — Schools
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Download{" "}
+                                        <a href="https://www.compare-school-performance.service.gov.uk/download-data">
+                                            here
+                                        </a>
+                                        . For our uses, we utilised data from Kent exclusively.
+                                    </td>
+                                    <td>2010–2025</td>
                                     <td>CSV</td>
                                     <td>Late 2025</td>
                                 </tr>
-
-                                {/* <!-- Flood Data --> */}
                                 <tr>
                                     <td>Flood</td>
                                     <td><a href="https://data.police.uk/data/">TO BE FILLED OUT</a></td>
@@ -86,8 +167,6 @@ const Content = () => {
                                     <td>XXX</td>
                                     <td>XXX</td>
                                 </tr>
-
-                                {/* <!-- Housing Price Data --> */}
                                 <tr>
                                     <td>House Pricing</td>
                                     <td><a href="https://data.police.uk/data/">TO BE FILLED OUT</a></td>
@@ -96,8 +175,6 @@ const Content = () => {
                                     <td>XXX</td>
                                     <td>XXX</td>
                                 </tr>
-
-                                {/* <!-- Housing Development Data --> */}
                                 <tr>
                                     <td>Housing Developments</td>
                                     <td><a href="https://data.police.uk/data/">TO BE FILLED OUT</a></td>
@@ -110,16 +187,67 @@ const Content = () => {
                         </table>
                     </div>
 
+                    {/* -------- Charts ---------- */}
                     <div className="contents-items">
-                        <h2>Charts</h2>
-                        <p>Here you will find information about the different charts available in AQOLA, and how to use them.</p>
+
+
+                        <h2 id="charts">Charts</h2>
+                        <div>
+                            Here you will find information about the different charts available
+                            in AQOLA, and how to use them.
+                        </div>
+                        <div>
+                            Charts come in two forms. Static and dynamic. Static graphs will 
+                            show data, no matter what areas you have selected. Dynamic will 
+                            update, based on the areas selected for that dataset.
+                        </div>
+                        <div>
+                            Charts will only update when they are selected (or focused). To 
+                            focus a chart, please click on it. The screen should update to 
+                            show any areas that graph has already, and then you can add/remove
+                            what you want.
+                        </div>
+
+
                         <h3>Bar Charts</h3>
-                        <p>Our bar charts are pretty standard. Mostly comprising of a count on the y-axis and a category on the x-axis. We include a legend on the side, to help distinguish between different bars, and we also include tooltips for the same reason.</p>
-                        <p>In some charts, we may group together bars along the x-axis. This is to distinguish between different categories. This usually falls down to the different areas you select</p>
+                        <div>
+                            Our bar charts are pretty standard — mostly comprising of a count
+                            on the y-axis and a category on the x-axis. We include a legend on
+                            the side to help distinguish between different bars, and tooltips
+                            for the same reason.
+                        </div>
+                        <div>
+                            In some charts, bars may be grouped along the x-axis to distinguish
+                            between different categories — this usually corresponds to the
+                            different areas you select.
+                        </div>
+
+
+                        <h3>Line Charts</h3>
+                        <div>
+                            Like our bar charts, the line charts are also pretty standard. 
+                            For most (if not all) charts, the y-axis shows a time period, 
+                            and the x-axis shows a count of some sort (i.e. crime count)
+                        </div>
+                        <div>
+                            A number of charts will have multiple lines. These will be to 
+                            represent certain categories or locations. These will be shown 
+                            in the legend at the side.
+                        </div>
+
+
+                        <h3>Spider Diagrams</h3>
+                        <div>
+                            These diagrams aim to show a shaded area, based on values for a 
+                            number of attributes. Each diagram will have a set of lines, 
+                            based on the number of attributes of the datasets and the area 
+                            is designed to show the overall performance of over all categories.
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-);};
+    );
+};
 
 export default Content;
