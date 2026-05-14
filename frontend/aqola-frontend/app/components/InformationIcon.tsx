93829f94ca998c5172013b219f722b693493e441
Andrew Meyer <am2660@kent.ac.uk>
2026-05-14 15:44:03 +0100

    work on icon

--- frontend/aqola-frontend/app/components/InformationIcon.tsx
+++ frontend/aqola-frontend/app/components/InformationIcon.tsx
@@ -1,34 +1,46 @@
 import { BadgeInfo } from "lucide-react";
 import { Tooltip } from "react-tooltip";
 import { useState } from "react";
-
 interface InformationalIconProps {
     content: any;
 }
 
+
+
 const InformationalIcon = ({ content }: InformationalIconProps) => {
-    const [boxHovered, setBoxHovered] = useState(false);
-    const [iconHovered, setIconHovered] = useState(false);
+    const [isOpen, setIsOpen] = useState(false)
+    const [isClicked, setIsClicked] = useState(false)
+
+    const openTest = () => {
+        if(!isClicked){
+            setIsOpen(false)
+        }
+    }
 
     return (
-        <>
+        <div 
+            onMouseOver={() => setIsOpen(true)}
+            onMouseOut={() => openTest()}
+            onMouseDown={() => setIsClicked(prev => !prev)}
+        >
             <BadgeInfo
-                data-tooltip-id={content.id + "info-tooltip"}
-                onMouseOver={() => setIconHovered(true)}
-                onMouseLeave={() => setIconHovered(false)}
+                data-tooltip-id={content.id + "-info-tooltip"}
             />
             <Tooltip
-                id={content.id + "info-tooltip"}
+                id={content.id + "-info-tooltip"}
                 className="tooltip"
-                style={{ maxWidth: "50%" }}
+                style={{ maxWidth: "50%", borderRadius:"10px"}}
                 delayHide={500}
+                clickable={true}
+                isOpen={isOpen}
+                
             >
                 <div>
                     <div>{content.blurb}</div>
                     <div style={{padding: "5px", fontStyle:"italic", fontSize:"10px"}}>For more information, please see documentation</div>
                 </div>
             </Tooltip>
-        </>
+        </div>
     );
 };
 
