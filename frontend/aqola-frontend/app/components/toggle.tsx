984f714103b2f9471075517f7c66738a5c776784
Andrew Meyer <am2660@kent.ac.uk>
2026-05-22 15:06:20 +0100

    fixed issue

--- frontend/aqola-frontend/app/components/toggle.tsx
+++ frontend/aqola-frontend/app/components/toggle.tsx
@@ -4,10 +4,11 @@ import { Tooltip } from "react-tooltip";
 
 function Toggle(){
   const toggleDatasetWideEditing = useAppStore((state) => state.toggleDatasetWideEditing)
+  const datasetWideEditing = useAppStore((state) => state.datasetWideEditing);
 
   return (
     <label className="relative inline-flex items-center cursor-pointer gap-3">
-      <input type="checkbox" className="sr-only peer" defaultChecked onChange={() => setTimeout(toggleDatasetWideEditing, 0)}/>
+      <input type="checkbox" className="sr-only peer" checked={datasetWideEditing} onChange={() => setTimeout(toggleDatasetWideEditing, 0)}/>
       <span className="relative w-11 h-6 bg-gray-200 rounded-full 
         peer-checked:bg-cyan-500
         after:content-[''] after:absolute after:top-[2px] after:left-[2px] 
